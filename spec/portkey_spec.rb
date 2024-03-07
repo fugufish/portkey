e044485f9233ec63be01bb0613a77a70fd9012bd
fugufish <fugu@hey.com>
2024-03-07 06:41:41 -0700

    initial commit

---
+++ spec/portkey_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Portkey do
+  it "has a version number" do
+    expect(Portkey::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
